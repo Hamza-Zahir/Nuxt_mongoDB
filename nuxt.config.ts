7df5b3af40c35cf7e5fd4989efd90dac718353f8
Hamza-Zahir <hamza19zahir98@gmail.com>
2023-01-29 16:37:36 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // runtimeConfig: {
-  //   public: {
-  //     // PORT: process.env.PORT || 8080,
-  //     // MONGODB_URL: process.env.MONGODB_URL,
-  //   },
-  // },
+  runtimeConfig: {
+      MONGODB_URL: process.env.MONGODB_URL,
+  },
   nitro: {
     plugins: ["~/server/index.ts"],
   },
-  // serverMiddleware: [
-  //   // Will register file from project server-middleware directory to handle /server-api/* requests
-  //   { path: "/server-api", handler: "~/serverMiddleware/index.ts" },
-  // ],
+  // build:{
+  //   transpile:["@heroicons/vue"]
+  // },
+
 });
--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,10 @@ import { Nitro } from "nitropack";
 import mongoose from "mongoose";
 
 export default async (_nitroApp: Nitro) => {
-  // const config = useAppConfig()
-  const MONGODB_URL =
-    "mongodb+srv://hamzahir:MDBazmah98@cluster0.geuf2sy.mongodb.net/amazonProducts?retryWrites=true&w=majority";
-  // "mongodb://http://localhost/27017/nitro"
+  const config = useRuntimeConfig()
+
+  const MONGODB_URL = config.MONGODB_URL 
   try {
-    // {useNewUrlParser: true}
     mongoose.set("strictQuery", false);
     await mongoose.connect(MONGODB_URL);
     console.log("connected to mongo");
